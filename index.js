ec8c259312a89443b6ca48e16b6b58cee87fb05c
Frank Steiler <frank@steilerdev.de>
2019-01-25 22:05:25 +0100

    Reworked accessory loading mechanism

--- index.js
+++ index.js
@@ -1,21 +1,11 @@
-const util = require("util");
+const fs = require('fs');
 
-const version = require('./package.json').version;
+const version = require('./package').version;
 const platformName = require('./package').name;
 const platformPrettyName = 'openHAB2-Complete';
 
 const SerialNumberGenerator = require('./util/SerialNumberGenerator');
 const {OpenHAB} = require('./util/OpenHAB');
-
-const SwitchAccessory = require("./accessory/Switch").createSwitchAccessory;
-const LightAccessory = require("./accessory/Light").createLightAccessory;
-const TemperatureSensorAccessory = require("./accessory/TemperatureSensor").createTemperaturSensorAccessory;
-const HumiditySensorAccessory = require("./accessory/HumiditySensor").createHumiditySensorAccessory;
-const ThermostatAccessory = require("./accessory/Thermostat").createThermostatAccessory;
-const WindowCoveringAccessory = require("./accessory/WindowCovering").createWindowCoveringAccessory;
-const MotionSensorAccessory = require("./accessory/MotionSensor").createMotionSensorAccessory;
-const LeakSensorAccessory = require("./accessory/LeakSensor").createLeakSensorAccessory;
-const COSensorAccessory = require("./accessory/COSensor").createCOSensorAccessory;
 
 module.exports = (homebridge) => {
     homebridge.registerPlatform(platformName, platformPrettyName, OpenHABComplete);
@@ -23,18 +13,6 @@ module.exports = (homebridge) => {
 
 const OpenHABComplete = class {
     constructor(log, config, api) {
-
-        this._factories = {
-            switch: SwitchAccessory,
-            light: LightAccessory,
-            temp: TemperatureSensorAccessory,
-            humidity: HumiditySensorAccessory,
-            thermostat: ThermostatAccessory,
-            windowcovering: WindowCoveringAccessory,
-            motion: MotionSensorAccessory,
-            leak: LeakSensorAccessory,
-            co: COSensorAccessory
-        };
 
         this._log = log;
         this._config = config;
@@ -54,6 +32,33 @@ const OpenHABComplete = class {
                 log: log
             };
         }
+
+        this._factories = {};
+
+        // Loading accessories from file system
+        this._log(`Loading accessories...`);
+        let accessoryDirectory = "./accessory";
+        let accessoryFiles = fs.readdirSync(accessoryDirectory, {"withFileTypes": true});
+        accessoryFiles.forEach(function (accessoryFile) {
+            if(accessoryFile.isFile()) {
+                let accessoryFilePath = `${accessoryDirectory}/${accessoryFile.name}`;
+
+                let accessoryType = require(accessoryFilePath).type;
+                if(accessoryType === undefined || !(typeof accessoryType === "string")) {
+                    this._log.warn(`Ignoring ${accessoryFilePath} due to missing 'type' definition`);
+                } else {
+                    this._log.debug(`Found accessory of type ${accessoryType}`);
+                    let accessoryFactory = require(accessoryFilePath).createAccessory;
+                    if(accessoryFactory === undefined || !(typeof accessoryFactory === "function")) {
+                        this._log.warn(`Ignoring ${accessoryFilePath}, due to missing 'createAccessory' definition`);
+                    } else {
+                        this._log(`Loading and activating accessory ${type}`);
+                        this._factories[type] = require(accessoryFilePath).createAccessory;
+                    }
+                }
+            }
+        });
+
         this._log.info(`'OpenHAB2 - Complete Edition' Plugin Loaded - Version ${version}`);
     }
 
@@ -62,20 +67,20 @@ const OpenHABComplete = class {
         const { accessories } = this._config;
         accessories.forEach(acc => {
             if (!(acc.type)) {
-                this._log.warn(`Invalid configuration: Accessory type is invalid: ${util.inspect(acc)}, skipping`);
+                this._log.warn(`Invalid configuration: Accessory type is invalid: ${JSON.stringify(acc)}, skipping`);
                 return;
             }
 
             const factory = this._factories[acc.type];
             if (factory === undefined) {
-                this._log.warn(`Invalid configuration: Accessory type is unknown: ${util.inspect(acc)}, skipping`);
+                this._log.warn(`Invalid configuration: Accessory type is unknown: ${JSON.stringify(acc)}, skipping`);
                 return;
             }
 
             if(acc.name) {
                 acc.serialNumber = SerialNumberGenerator.generate(acc.name);
             } else {
-                this._log.warn(`Invalid configuration: Accessory name is unknown: ${util.inspect(acc)}, skipping`);
+                this._log.warn(`Invalid configuration: Accessory name is unknown: ${JSON.stringify(acc)}, skipping`);
                 return;
             }
 
