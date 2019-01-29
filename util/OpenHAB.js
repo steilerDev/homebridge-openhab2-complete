dc3357397b28bb52b9b0dc8f9c809a3f7b7f5f0b
Frank Steiler <frank@steilerdev.de>
2019-01-29 14:45:53 +0100

    Trying to recover from closed subscription connections

--- util/OpenHAB.js
+++ util/OpenHAB.js
@@ -113,6 +113,7 @@ class OpenHAB {
 
     subscribe(habItem, callback) {
         let myURL = clone(this._url);
+        const CLOSED = 2;
         myURL.pathname = '/rest/events';
         myURL.search = `topics=smarthome/items/${habItem}/statechanged`;
         let source = new EventSource(myURL.href);
@@ -125,13 +126,24 @@ class OpenHAB {
             }
         };
         source.onerror = function (err) {
-            if(err.message) {
-                if(err.status) {
-                    callback(new Error(`${err.status}: ${err.message}`));
-                } else {
-                    callback(new Error(err.message));
+                if(err.message) {
+                    if(err.status) {
+                        callback(new Error(`${err.status}: ${err.message}`));
+                    } else {
+                        callback(new Error(err.message));
+                    }
+                    if(source.readyState === CLOSED || err.status === 404) {
+                        callback(new Error(`Subscription closed for ${habItem}, trying to reconnect in 1sec...`));
+                        setTimeout(function() {
+                            callback(new Error(`Trying to reconnect subscription for ${habItem}...`);
+                            source.close();
+                            subscribe(habItem, callback);
+                        }, 1000);
+                    }
                 }
-            }
+            };
+
+
         };
         return source;
     }
